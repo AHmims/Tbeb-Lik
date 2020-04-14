3b0bbaffe1851a6f24e169209f8cfb700f0bf959
AHmims <ali.hmims99@gmail.com>
2020-04-14 12:52:56 +0100

    init

--- admin/javascript/file.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("Ajoute").addEventListener("click",()=>{
-    document.getElementsByClassName("bg-modal")[0].style.display = "block";
-});
-document.getElementsByClassName("close")[0].addEventListener("click",()=>{
-    document.getElementsByClassName("bg-modal")[0].style.display = "none";
-});
