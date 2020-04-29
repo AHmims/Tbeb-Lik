8495477db5c4d3a9674d3c7a2ef19497894ece1d
Z-Kamili <zakariakamili97@gmail.com>
2020-04-29 15:27:28 +0000

    modification file load

--- admin/javascript/file.js
+++ admin/javascript/file.js
@@ -4,3 +4,11 @@ document.getElementById("Ajoute").addEventListener("click",()=>{
 document.getElementsByClassName("close")[0].addEventListener("click",()=>{
     document.getElementsByClassName("bg-modal")[0].style.display = "none";
 });
+var i = 0;
+document.getElementById("btnLogin").addEventListener("click",()=>{
+         i++;
+    document.getElementById("btnLogin").value = "Veuillez attendez...";
+    if(i>=2){
+document.getElementById("btnLogin").disabled = "true";
+    }
+});
